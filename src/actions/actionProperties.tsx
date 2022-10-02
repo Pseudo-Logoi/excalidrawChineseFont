a8c1fe144b4955c1bc9283c29ea26a1db325acef
Walter Wong <wanghy7317@163.com>
2022-10-02 20:52:54 +0800

    添加 FontFamilyChineseIcon

--- src/actions/actionProperties.tsx
+++ src/actions/actionProperties.tsx
@@ -16,6 +16,7 @@ import {
   FontFamilyCodeIcon,
   FontFamilyHandDrawnIcon,
   FontFamilyNormalIcon,
+  FontFamilyChineseIcon,
   FontSizeExtraLargeIcon,
   FontSizeLargeIcon,
   FontSizeMediumIcon,
